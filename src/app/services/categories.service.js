43e13b4c69a206649e3d0cdc6d6987cf6875fedb
Beliars <2beliars@gmail.com>
2016-12-28 17:23:45 +0200

    categories fixes

--- src/app/services/categories.service.js
+++ src/app/services/categories.service.js
@@ -17,70 +17,24 @@ export class CategoriesService {
                         "id": 3,
                         "name": "SubCat 1.2",
                         "children": []
-                    },
-                    {
-                        "id": 4,
-                        "name": "SubCat 1.3",
-                        "children": []
-                    },
-                    {
-                        "id": 5,
-                        "name": "SubCat 1.4",
-                        "children": [
-                            {
-                                "id": 6,
-                                "name": "SubCat 1.4.1",
-                                "children": []
-                            },
-                            {
-                                "id": 7,
-                                "name": "SubCat 1.4.2",
-                                "children": []
-                            }
-                        ]
                     }
                 ]
             },
             {
-                "id": 8,
+                "id": 4,
                 "name": "Cat 2",
                 "children": [
                     {
-                        "id": 9,
+                        "id": 5,
                         "name": "SubCat 2.1",
                         "children": []
-                    },
-                    {
-                        "id": 10,
-                        "name": "SubCat 2.2",
-                        "children": []
-                    },
-                    {
-                        "id": 11,
-                        "name": "SubCat 2.3",
-                        "children": []
-                    },
-                    {
-                        "id": 12,
-                        "name": "SubCat 2.4",
-                        "children": [
-                            {
-                                "id": 13,
-                                "name": "SubCat 2.4.1",
-                                "children": []
-                            },
-                            {
-                                "id": 14,
-                                "name": "SubCat 2.4.2",
-                                "children": []
-                            }
-                        ]
                     }
                 ]
             }
         ];
 
-        // localStorage.setItem('notesAppCategories', JSON.stringify(this.mockCategories));
+
+        localStorage.setItem('notesAppCategories', JSON.stringify(this.mockCategories));
 
     }
 
